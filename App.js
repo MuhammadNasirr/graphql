7cafc282842ba6b4a67abeed1e89f227d6fb7c3b
mughees605 <mughees605@gmail.com>
2018-09-19 03:40:13 +0500

    added aws appsync

--- App.js
+++ App.js
@@ -1,32 +1,37 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+
 
 import React, { Component } from 'react';
 import { Platform, StyleSheet, Text, View, ScrollView, TouchableOpacity, TextInput } from 'react-native';
 import { Icon, ListItem } from 'react-native-elements';
 import { PrimaryStack } from './src/navigation'
+// appsync work
+import AWSAppSyncClient from "aws-appsync";
+import { Rehydrated } from 'aws-appsync-react';
+import { ApolloProvider } from 'react-apollo';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
+import appSyncConfig from './aws-exports';
+
+const client = new AWSAppSyncClient({
+  url: appSyncConfig.graphqlEndpoint,
+  region: appSyncConfig.region,
+  auth: {
+    type: appSyncConfig.authType,
+    apiKey: appSyncConfig.apiKey,
+  }
 });
 
-type Props = {};
-export default class App extends Component<Props> {
+export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = { text: 'Search' };
   }
   render() {
     return (
-      <PrimaryStack />
+      <ApolloProvider client={client}>
+        <Rehydrated>
+          <PrimaryStack />
+        </Rehydrated>
+      </ApolloProvider>
     );
   }
 }
