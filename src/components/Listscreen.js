354c7a9aff0c9c4134eb53b32b559d657f668f20
mughees605 <mughees605@gmail.com>
2018-09-19 03:40:35 +0500

    get product list

--- src/components/Listscreen.js
+++ src/components/Listscreen.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import { Platform, StyleSheet, Text, View, ScrollView, TouchableOpacity, TextInput } from 'react-native';
 import { Icon, ListItem } from 'react-native-elements';
-import { ProfileNavBar } from '../components'
-
+import { ProfileNavBar } from '../components';
+import { Query } from 'react-apollo';
+import listProduct from './../queries/listProduct';
 
 export default class Listscreen extends Component {
     static navigationOptions = {
@@ -21,38 +22,7 @@ export default class Listscreen extends Component {
         const inputAccessoryViewID = "uniqueID";
         const { activeTab } = this.state;
         const { navigate } = this.props.navigation;
-        const list = [
-            {
-                name: 'Hair, Skin & Nails Formula',
-                avatar_url: 'https://cache.pakistantoday.com.pk/5c6NgsBa.jpeg',
-                subtitle: 'Neque porro quisquam est qui dolorem ipsum quia dolor sit'
-            },
-            {
-                name: 'Hair, Skin & Nails Formula',
-                avatar_url: 'https://cache.pakistantoday.com.pk/5c6NgsBa.jpeg',
-                subtitle: 'Neque porro quisquam est qui dolorem ipsum quia dolor sit'
-            },
-            {
-                name: 'Hair, Skin & Nails Formula',
-                avatar_url: 'https://cache.pakistantoday.com.pk/5c6NgsBa.jpeg',
-                subtitle: 'Neque porro quisquam est qui dolorem ipsum quia dolor sit'
-            },
-            {
-                name: 'Hair, Skin & Nails Formula',
-                avatar_url: 'https://cache.pakistantoday.com.pk/5c6NgsBa.jpeg',
-                subtitle: 'Neque porro quisquam est qui dolorem ipsum quia dolor sit'
-            },
-            {
-                name: 'Hair, Skin & Nails Formula',
-                avatar_url: 'https://cache.pakistantoday.com.pk/5c6NgsBa.jpeg',
-                subtitle: 'Neque porro quisquam est qui dolorem ipsum quia dolor sit'
-            },
-            {
-                name: 'Hair, Skin & Nails Formula',
-                avatar_url: 'https://cache.pakistantoday.com.pk/5c6NgsBa.jpeg',
-                subtitle: 'Neque porro quisquam est qui dolorem ipsum quia dolor sit'
-            },
-        ]
+        
         return (
             <View style={styles.container}>
                 <View style={{ marginTop: 10, backgroundColor: '#fff', width: '90%', borderRadius: 10, justifyContent: 'space-between', flexDirection: 'row' }}>
@@ -79,39 +49,47 @@ export default class Listscreen extends Component {
                 <ScrollView style={{ width: '90%', marginVertical: 10 }}>
                     <ScrollView style={{}}>
                         <View containerStyle={{ backgroundColor: 'white', }}>
-                            {
-                                list.map((l, i) => (
-                                    <TouchableOpacity  onPress={() => navigate('ListDetail')}>
+                            <Query query={listProduct}>
+                                {({ loading, error, data }) => {
+                                    console.log(data)
+                                    if (loading) return <Text>...loading</Text>;
+                                    if (error) return <Text>{error.message}</Text>;
 
-                                        <ListItem
-                                            // onPress={() => navigate('MCHProjectScreen')}
-                                            containerStyle={{
-                                                borderBottomColor: 'transparent',
-                                                marginBottom: 10,
-                                                height: 100,
-                                                backgroundColor: '#fff', borderRadius: 10,
-                                            }}
-                                            key={i}
-                                            hideChevron
-                                            titleStyle={{
-                                                marginLeft: 15,
-                                                // marginTop:-30,
-                                                fontWeight: 'bold',
-                                                fontSize: 18
-                                            }}
+                                    {
+                                       return data.getProducts.map((l, i) => (
+                                            <TouchableOpacity onPress={() => navigate('ListDetail')}>
 
-                                            subtitleStyle={{ fontSize: 16, marginLeft: 15, height: 50 }}
-                                            avatarContainerStyle={{ width: 70, height: 70 }}
-                                            subtitleContainerStyle={{}}
-                                            avatarStyle={{ borderColor: '#c5c5c5', borderWidth: 2, width: 75, height: 75, borderRadius: 10, }}
-                                            avatar={source = { uri: l.avatar_url }}
-                                            subtitleNumberOfLines={3}
-                                            title={l.name}
-                                            subtitle={l.subtitle}
-                                        />
-                                    </TouchableOpacity>
-                                ))
-                            }
+                                                <ListItem
+                                                    // onPress={() => navigate('MCHProjectScreen')}
+                                                    containerStyle={{
+                                                        borderBottomColor: 'transparent',
+                                                        marginBottom: 10,
+                                                        height: 100,
+                                                        backgroundColor: '#fff', borderRadius: 10,
+                                                    }}
+                                                    key={i}
+                                                    hideChevron
+                                                    titleStyle={{
+                                                        marginLeft: 15,
+                                                        // marginTop:-30,
+                                                        fontWeight: 'bold',
+                                                        fontSize: 18
+                                                    }}
+
+                                                    subtitleStyle={{ fontSize: 16, marginLeft: 15, height: 60 }}
+                                                    avatarContainerStyle={{ width: 70, height: 70 }}
+                                                    subtitleContainerStyle={{}}
+                                                    avatarStyle={{ borderColor: '#c5c5c5', borderWidth: 2, width: 75, height: 75, borderRadius: 10, }}
+                                                    avatar={source = { uri: l.image_url }}
+                                                    subtitleNumberOfLines={3}
+                                                    title={l.title}
+                                                    subtitle={l.description}
+                                                />
+                                            </TouchableOpacity>
+                                        ))
+                                    }
+                                }}
+                            </Query>
                         </View>
                     </ScrollView>
                 </ScrollView>
