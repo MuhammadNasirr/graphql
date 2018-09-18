d9bf43e3a3630542ac0d5773577353d39607b968
mughees605 <mughees605@gmail.com>
2018-09-19 01:33:49 +0500

    aws credentials for appsync

---
+++ aws-exports.js
@@ -0,0 +1,8 @@
+
+export default {
+    "graphqlEndpoint": "https://nmhh33lqanhzxevajeesvkrd2q.appsync-api.us-east-1.amazonaws.com/graphql",
+    "region": "us-east-1",
+    "apiKey": "da2-bcnyuhl3lzfork7ndvajmhgkym",
+    "authType": "API_KEY"
+  }
+  
